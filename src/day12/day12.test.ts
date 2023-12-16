a8663e08d133497c3cea45254622d97d4e5afc6b
robcronin <robc@theodo.co.uk>
2023-12-16 14:17:17 +0000

    pt 1

---
+++ src/day12/day12.test.ts
@@ -0,0 +1,71 @@
+import { logAnswer } from '../utils/logging';
+import { day12, day12part2, getIsValidRecord, getNumValidPerms, getPerms } from './day12';
+import { data, testData } from './day12.data';
+
+describe('day 12', () => {
+  it('test cases', () => {
+    expect(day12(testData)).toBe(21);
+  });
+
+  it('answer', () => {
+    const answer = day12(data);
+    logAnswer(answer, 12, 1);
+    expect(answer).toBe(7379);
+  });
+});
+
+describe.skip('day 12 part 2', () => {
+  it('test cases', () => {
+    expect(day12part2(testData)).toBe(12);
+  });
+
+  it('answer', () => {
+    const answer = day12part2(data);
+    logAnswer(answer, 12, 2);
+    expect(answer).toBe(12);
+  });
+});
+
+describe('getIsValidRecord', () => {
+  it('should return true for valid records', () => {
+    expect(getIsValidRecord({ springs: '#.#.###'.split(''), groups: [1, 1, 3] })).toBe(true);
+    expect(getIsValidRecord({ springs: '#.#.##'.split(''), groups: [1, 1, 2] })).toBe(true);
+    expect(getIsValidRecord({ springs: '..#.#.##'.split(''), groups: [1, 1, 2] })).toBe(true);
+    expect(getIsValidRecord({ springs: '..##.#.##.'.split(''), groups: [2, 1, 2] })).toBe(true);
+  });
+  it('should return false for invalid records', () => {
+    expect(getIsValidRecord({ springs: '#.#.##'.split(''), groups: [1, 1, 3] })).toBe(false);
+    expect(getIsValidRecord({ springs: '#.#.###'.split(''), groups: [1, 1, 2] })).toBe(false);
+  });
+});
+
+describe('getPerms', () => {
+  it('should get the perms of springs', () => {
+    expect(getPerms(0, 0)).toEqual([]);
+    expect(getPerms(1, 1)).toEqual([['#']]);
+    expect(getPerms(2, 1)).toEqual([
+      ['.', '#'],
+      ['#', '.'],
+    ]);
+    expect(getPerms(3, 2)).toEqual([
+      ['.', '#', '#'],
+      ['#', '.', '#'],
+      ['#', '#', '.'],
+    ]);
+  });
+});
+
+describe('getNumValidPerms', () => {
+  it('should return the valid perms for a record', () => {
+    expect(getNumValidPerms({ springs: '???.###'.split(''), groups: [1, 1, 3] })).toBe(1);
+    expect(getNumValidPerms({ springs: '.??..??...?##.'.split(''), groups: [1, 1, 3] })).toBe(4);
+    expect(getNumValidPerms({ springs: '?#?#?#?#?#?#?#?'.split(''), groups: [1, 3, 1, 6] })).toBe(
+      1,
+    );
+    expect(getNumValidPerms({ springs: '????.#...#...'.split(''), groups: [4, 1, 1] })).toBe(1);
+    expect(getNumValidPerms({ springs: '????.######..#####.'.split(''), groups: [1, 6, 5] })).toBe(
+      4,
+    );
+    expect(getNumValidPerms({ springs: '?###????????'.split(''), groups: [3, 2, 1] })).toBe(10);
+  });
+});
---
+++ src/day12/day12.ts
@@ -0,0 +1,80 @@
+import { sumArr } from '../utils/array';
+import { range } from '../utils/looping';
+
+enum Spring {
+  OPERATIONAL = '.',
+  DAMAGED = '#',
+  UNKNOWN = '?',
+}
+type Record = { springs: Spring[]; groups: number[] };
+
+const parseRecords = (input: string[]): Record[] =>
+  input.map((line) => {
+    const [springs, groups] = line.split(' ');
+    return { springs: [...springs] as Spring[], groups: groups.split(',').map(Number) };
+  });
+
+export const getIsValidRecord = (record: Record): boolean => {
+  const { springs, groups } = record;
+  if (springs.includes(Spring.UNKNOWN)) return false;
+  const actualGroups: number[] = [];
+  let groupSize = 0;
+  for (let i = 0; i < springs.length; i++) {
+    const spring = springs[i];
+    if (spring === Spring.DAMAGED) groupSize++;
+    else if (groupSize > 0) {
+      actualGroups.push(groupSize);
+      groupSize = 0;
+    }
+  }
+  if (groupSize > 0) actualGroups.push(groupSize);
+
+  return (
+    groups.length === actualGroups.length &&
+    groups.every((group, index) => group === actualGroups[index])
+  );
+};
+
+export const getPerms = (num: number, requiredNumDamaged: number) => {
+  if (num === 0) return [];
+  let perms: Spring[][] = [[Spring.DAMAGED], [Spring.OPERATIONAL]];
+  range(num - 1).forEach(() => {
+    perms = [...perms.map((perm) => [...perm]), ...perms.map((perm) => [...perm])];
+    perms.forEach((perm, index) => {
+      perm.push(index < perms.length / 2 ? Spring.DAMAGED : Spring.OPERATIONAL);
+    });
+  });
+  return perms.filter((perm) => {
+    const currentNumDamaged = perm.filter((spring) => spring === Spring.DAMAGED).length;
+    return currentNumDamaged === requiredNumDamaged;
+  });
+};
+
+export const getNumValidPerms = (record: Record): number => {
+  const { springs, groups } = record;
+  const unknowns = springs
+    .map((spring, index) => spring === Spring.UNKNOWN && index)
+    .filter((index) => index !== false) as number[];
+
+  const currentNumDamaged = springs.filter((spring) => spring === Spring.DAMAGED).length;
+  const requiredNumDamaged = sumArr(groups, (i) => i);
+
+  const perms = getPerms(unknowns.length, requiredNumDamaged - currentNumDamaged);
+
+  const validPerms = perms.filter((perm) => {
+    const fixedSprings = [...springs];
+    unknowns.forEach((unknownIndex, index) => (fixedSprings[unknownIndex] = perm[index]));
+    return getIsValidRecord({ springs: fixedSprings, groups });
+  });
+
+  return validPerms.length;
+};
+
+export const day12 = (input: string[]) => {
+  const records = parseRecords(input);
+  return sumArr(records, getNumValidPerms);
+};
+
+export const day12part2 = (input: string[]) => {
+  return 12;
+};
